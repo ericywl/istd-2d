2e68e970032e87aa85453f6fbc2098c193261b47
ericywl <midnight2903@gmail.com>
2017-11-03 01:50:34 +0800

    commiting old satsolver

--- InfoSys/sat_2d/src/main/java/test/SATSolverTestOld.java
+++ InfoSys/sat_2d/src/main/java/test/SATSolverTestOld.java
@@ -9,7 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 
-import sat.list.ReadCNF;
+import sat.ReadCNF;
 import sat.twoSat.BooleanAssignment;
 import sat.twoSat.CNFParser;
 import sat.twoSat.SATSolver2;
